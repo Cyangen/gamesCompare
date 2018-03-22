6b644bce93cca9d5c347c87e6b7ff6363008fe9d
Matteo <matteoratti90@mail.com>
2018-03-22 18:41:57 +0100

    scrip base to get info from sites

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'nokogiri', '~> 1.6', '>= 1.6.8'
 gem 'mechanize', '~> 2.7', '>= 2.7.4'
+gem 'similar_text', '~> 0.0.4'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,8 +4,75 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+
+    if params[:game_search]
+      game_search_term = params[:game_search]
+      mechanize = Mechanize.new
+
+      # INSTANT GAMING SEARCH
+      instant_game_page = mechanize.get('https://www.instant-gaming.com/it/')
+      search_input = instant_game_page.forms.first
+      search_input['q'] = game_search_term
+      instant_game_page = search_input.submit
+      results = instant_game_page.at('.search')
+      game = results.search('div')[0]
+
+      game_price = game.at('.price')
+      game_name = game.at('.name')
+      game_platform = game.at('.badge').attr('class')
+      game_link = game.at('.cover')["href"]
+      game_image = game.at('.picture.mainshadow')["src"]
+
+      puts '0000000000000000000000 INSTANT GAMING RESULT 00000000000000000000000000000000000'
+      puts game_price.text.strip
+      puts game_name.text.strip
+      puts game_link
+      puts game_image
+      puts game_platform.remove('badge').strip
+      puts '00000000000000000000000000 END INSTANT GAMING RESULT 0000000000000000000000000000000'
+
+
+    #KINGUIN SEARCH
+      # kinguin_page = mechanize.get('https://www.kinguin.net')
+      # search_input = kinguin_page.forms.first
+      # search_input['q'] = game_search_term
+      # kinguin_page = search_input.submit
+      #
+      # results = kinguin_page.at('.cat-v1')
+      # offer_details = results.at('#offerDetails')
+      #
+      # div_game = offer_details.search('div')[0]
+      #
+      # div_price = div_game.at('.actual-price')
+      # div_name = div_game.at('.product-name')
+      #
+      # puts '0000000000000000000000 KINGUIN RESULT 00000000000000000000000000000000000'
+      # puts div_name.text.strip
+      # puts div_price.text.strip
+      # puts '00000000000000000000000000 END KINGUIN RESULT 0000000000000000000000000000000'
+
+    #G2A SEARCH
+      # g2a_page = mechanize.get('https://www.g2a.com/')
+      # search_input = g2a_page.forms.first
+      # search_input['query'] = game_search_term
+      # g2a_page = search_input.submit
+      #
+      # results = g2a_page.at('.products-grid')
+      # div_game = results.search('li')[0]
+      #
+      # div_price = div_game.at('.Card__price')
+      # div_name = div_game.at('.Card__title')
+      #
+      # puts '0000000000000000000000 G2A RESULT 00000000000000000000000000000000000'
+      # puts div_name.text.strip
+      # puts div_price.text.strip
+      # puts '00000000000000000000000000 END G2A RESULT 0000000000000000000000000000000'
+
+    end
+
   end
+
+
 
   # GET /games/1
   # GET /games/1.json
