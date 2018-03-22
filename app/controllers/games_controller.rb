b98283d8e3e4b8d84f3460c858438dbfd80c2b9c
Matteo <matteoratti90@mail.com>
2018-03-22 20:53:02 +0100

    advanced script search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,58 +15,93 @@ class GamesController < ApplicationController
       search_input['q'] = game_search_term
       instant_game_page = search_input.submit
       results = instant_game_page.at('.search')
-      game = results.search('div')[0]
+      games = results.search('.item')
 
-      game_price = game.at('.price')
-      game_name = game.at('.name')
-      game_platform = game.at('.badge').attr('class')
-      game_link = game.at('.cover')["href"]
-      game_image = game.at('.picture.mainshadow')["src"]
+      count_elements = 0
+      instant_game_array = []
+
+      games.each do |game|
+
+        game_price = game.at('.price').text.strip
+        game_name = game.at('.name').text.strip
+        game_platform = game.at('.badge').attr('class').remove('badge').strip
+        game_link = game.at('.cover')["href"]
+        game_image = game.at('.picture.mainshadow')["src"]
+
+        hash =  {price:game_price,name:game_name,platform:game_platform,link:game_link,image:game_image}
+        instant_game_array << hash
+
+        count_elements = count_elements + 1
+      end
 
       puts '0000000000000000000000 INSTANT GAMING RESULT 00000000000000000000000000000000000'
-      puts game_price.text.strip
-      puts game_name.text.strip
-      puts game_link
-      puts game_image
-      puts game_platform.remove('badge').strip
+      puts instant_game_array
       puts '00000000000000000000000000 END INSTANT GAMING RESULT 0000000000000000000000000000000'
 
-
     #KINGUIN SEARCH
-      # kinguin_page = mechanize.get('https://www.kinguin.net')
-      # search_input = kinguin_page.forms.first
-      # search_input['q'] = game_search_term
-      # kinguin_page = search_input.submit
-      #
-      # results = kinguin_page.at('.cat-v1')
-      # offer_details = results.at('#offerDetails')
-      #
-      # div_game = offer_details.search('div')[0]
-      #
-      # div_price = div_game.at('.actual-price')
-      # div_name = div_game.at('.product-name')
-      #
-      # puts '0000000000000000000000 KINGUIN RESULT 00000000000000000000000000000000000'
-      # puts div_name.text.strip
-      # puts div_price.text.strip
-      # puts '00000000000000000000000000 END KINGUIN RESULT 0000000000000000000000000000000'
+      kinguin_page = mechanize.get('https://www.kinguin.net')
+      search_input = kinguin_page.forms.first
+      search_input['q'] = game_search_term
+      kinguin_page = search_input.submit
+
+      results = kinguin_page.at('.cat-v1')
+      offer_details = results.at('#offerDetails')
+
+      games = offer_details.search('.category')
+
+      kinguin_games_array = []
+
+      games.each do |game|
+
+        game_price = game.at('.actual-price').text.strip
+        game_name = game.at('.product-name').text.strip
+
+        game_platform_container = game.at('.properties')
+        game_platform = game_platform_container.at('div')['title']
+
+        game_link_container = game.at('.product-name')
+        game_link_h4 = game_link_container.at('h4')
+        game_link = game_link_h4.at('a')["href"]
+
+        game_image_container = game.at('.flag')
+        game_image = game_image_container.at('img')["src"]
+
+        hash =  {price:game_price,name:game_name,platform:game_platform.remove('Platform: '),link:game_link,image:game_image}
+        kinguin_games_array << hash
+      end
+
+      puts '0000000000000000000000 KINGUIN RESULT 00000000000000000000000000000000000'
+      puts kinguin_games_array
+      puts '00000000000000000000000000 END KINGUIN RESULT 0000000000000000000000000000000'
 
     #G2A SEARCH
-      # g2a_page = mechanize.get('https://www.g2a.com/')
-      # search_input = g2a_page.forms.first
-      # search_input['query'] = game_search_term
-      # g2a_page = search_input.submit
-      #
-      # results = g2a_page.at('.products-grid')
-      # div_game = results.search('li')[0]
-      #
-      # div_price = div_game.at('.Card__price')
-      # div_name = div_game.at('.Card__title')
-      #
-      # puts '0000000000000000000000 G2A RESULT 00000000000000000000000000000000000'
-      # puts div_name.text.strip
-      # puts div_price.text.strip
-      # puts '00000000000000000000000000 END G2A RESULT 0000000000000000000000000000000'
+      g2a_page = mechanize.get('https://www.g2a.com/')
+      search_input = g2a_page.forms.first
+      search_input['query'] = game_search_term
+      g2a_page = search_input.submit
+
+      results = g2a_page.at('.products-grid')
+      games = results.search('li')
+
+      g2a_games_array = []
+
+      games.each do |game|
+        game_price = game.at('.Card__price').text.strip
+        game_name = game.at('.Card__title').text.strip
+
+        game_link_container = game.at('.Card__media')
+        game_link = game_link_container.at('a')['href']
+
+        game_image = game.at('.Card__img.Card__img--placeholder')['src']
+
+        hash =  {price:game_price,name:game_name,platform:nil,link:game_link,image:game_image}
+        g2a_games_array << hash
+
+      end
+
+      puts '0000000000000000000000 G2A RESULT 00000000000000000000000000000000000'
+      puts g2a_games_array
+      puts '00000000000000000000000000 END G2A RESULT 0000000000000000000000000000000'
 
     end
 
